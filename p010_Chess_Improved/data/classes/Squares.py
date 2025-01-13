1847ca169ed4a408b71d466bac7e39b42c9ec4c3
Aman Prajapati <pandaanimal55555@gmail.com>
2025-01-13 10:46:13 +0530

    Create Squares.py

---
+++ p010_Chess_Improved/data/classes/Squares.py
@@ -0,0 +1,43 @@
+# /* Square.py
+import pygame
+
+# Tile creator
+class Squares:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.abs_x = x * width
+        self.abs_y = y * height
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.pos = (x, y)
+        self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+        self.draw_color = (238,238,210) if self.color == 'light' else (118,150,86)
+        self.highlight_color = (186,202,68) if self.color == 'light' else (66,114,0)
+        self.occupying_piece = None
+        self.coord = self.get_coord()
+        self.highlight = False
+        self.rect = pygame.Rect(
+            self.abs_x,
+            self.abs_y,
+            self.width,
+            self.height
+        )
+
+    # get the formal notation of the tile
+    def get_coord(self):
+        columns = 'abcdefgh'
+        return columns[self.x] + str(self.y + 1)
+
+    def draw(self, display):
+        # configures if tile should be light or dark or highlighted tile
+        if self.highlight:
+            pygame.draw.rect(display, self.highlight_color, self.rect)
+        else:
+            pygame.draw.rect(display, self.draw_color, self.rect)
+        # adds the chess piece icons
+        if self.occupying_piece != None:
+            centering_rect = self.occupying_piece.img.get_rect()
+            centering_rect.center = self.rect.center
+            display.blit(self.occupying_piece.img, centering_rect.topleft)
